738579a60db8d762a5a75dfb47b83fcbae65cdf8
Stephin95 <stephin_thomas@protonmail.com>
2022-02-17 23:27:32 +0000

    Initial

---
+++ cron_script.sh
@@ -0,0 +1,32 @@
+TH="`dirname \"$0\"`"              # relative
+LOG_PATH="`( cd \"$MY_PATH\" && cd .. && pwd )`/pi/log/network.log"
+now=$(date +"%m-%d %r")
+
+# Which Interface do you want to check
+wlan='wlan0'
+# Which address do you want to ping to see if you can connect
+pingip='google.com'
+
+# Perform the network check and reset if necessary
+# /bin/ping -c 2 -I $wlan $pingip > /dev/null 2> /dev/null
+# if [ $? -ge 1 ] ; then
+#     echo "$now : No connection Retrying" >> $LOG_PATH
+#     /sbin/ifdown $wlan
+#     sleep 5
+#     /sbin/ifup --force $wlan
+# else
+#     echo "$now Network is UP. Just exit the program." >> $LOG_PATH
+#     echo "hahahaa"
+# fi
+
+# Performing network check
+/bin/ping -c 2 -I $wlan $pingip > /dev/null 2> /dev/null
+while [ $? -ge 1 ];
+do
+    echo "$now : Network is DOWN Retrying" >> $LOG_PATH
+    /sbin/ifdown $wlan
+    sleep 5
+    /sbin/ifup --force $wlan
+    /bin/ping -c 2 -I $wlan $pingip > /dev/null 2> /dev/null
+done
+echo "$now : Connection Successful" >> $LOG_PATH
